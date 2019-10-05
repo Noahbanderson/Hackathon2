224e9ce67ae1a7db58eb927db1dcadb961509ad3
Cody Nguyen <codyjimnguyen@gmail.com>
2019-10-05 10:45:45 -0600

    updated controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,40 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :destroy]
+  before_action :authenticate_user!
+  
+  
   def index
+    render json: Comment.all
   end
 
   def create
+    comment = Comment.new(comment_params)
+    comment.user_id = params[:current_user.id]
+    if comment.save
+      render json:comment
+    else
+      render json: comment.errors, status 422
+    end
   end
 
-  def update
-  end
+  #def update
+  #end
 
   def destroy
+    @comment.destroy
+  end
+
+  def show
+    render json: @comment
+  end
+
+private
+
+  def set_comment
+    @comment = current_user.videos.comments.find(params[:id])
+  end
+
+  def video_params
+    params.require(:comment).permit(:body,)
   end
 end
--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,13 +1,36 @@
 class VideosController < ApplicationController
+  before_action: set_video, only: [:show :destroy]
+  before_action: before_action :authenticate_user!
+  
   def index
+    render json: Video.all
   end
 
-  def update
+  def show
+  render json: @video
   end
 
   def create
+    video = Video.new(video_params)
+    video.user.id = params[:current_user.id]
+    if video.save
+      render json: video
+    else
+      render json: video.errors, status: 422
+    end
   end
 
   def destroy
+    @video.destroy
   end
+
+  private
+    def set_video
+      @video = current.user.videos.find(params[:id])
+    end
+
+    def video_params
+      params.require(:video).permit(:url, :title, :duration, :genre, :description)
+    end
+
 end
